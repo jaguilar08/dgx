2f403002444f3036ed59d58ceb61ccf81a1b5b8c
Jeff Aguilar <jaguilar@dealergeek.com>
2021-08-10 19:31:09 -0500

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Crea facilmente una nueva dealergeek api
---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+    name='dgx',
+    packages=['dgx'],
+    version='0.1',
+    license='MIT',
+    description='Dealergeek API',
+    author='Jeff Aguilar',
+    author_email='jaguilar@dealergeek.com',
+    url='https://github.com/jaguilar08/dgx',
+    download_url='https://github.com/jaguilar08/dgx/archive/refs/heads/master.zip',
+    keywords=['DEALERGEEK', 'API'],
+    install_requires=[
+        'werkzeug',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
