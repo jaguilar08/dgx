b47e021672a195e0f3120b9fc3c2293e9223f3a2
Jeff Aguilar <jaguilar@dealergeek.com>
2021-08-10 19:49:14 -0500

    cambio de mod

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name='dgx',
     packages=['dgx'],
-    version='0.1',
+    version='0.1.1',
     license='MIT',
     description='Dealergeek API',
     author='Jeff Aguilar',
